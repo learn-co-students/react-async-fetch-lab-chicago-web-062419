14f829d1918e0d7816e4a4a640df6050f6b098a2
Amanda Treutler <amandathedev@gmail.com>
2019-09-01 13:59:04 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {
+    peopleInSpace: []
+  };
+
+  // TODO Render
+  render() {
+    return <div></div>;
+  }
+
+  // TODO componentDidMount with fetch
+  componentDidMount() {
+    fetch("http://api.open-notify.org/astros.json")
+      .then(resp => resp.json())
+      .then(json => console.log(json));
+  }
+}
+
+export default App;
